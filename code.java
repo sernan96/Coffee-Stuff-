0cd61ae9aa045fb2b8cec9e21f686a681bce2aa3
Hwang gend <80672346+sernan96@users.noreply.github.com>
2023-11-16 23:43:10 +0900

    [UPDATE] Panel 구현
    CenterPanel에 게이지, 재료이름, 이미지 추가
    +)해야할 것 
    => 버튼 상호작용 기능

--- code.java
+++ code.java
@@ -1,12 +1,10 @@
-import org.w3c.dom.Text;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 
 public class Main extends JFrame{
     public Main(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("java programming report");
         Container c = getContentPane();
         c.add(new NorthPanel(), BorderLayout.NORTH);
         c.add(new CenterPanel(), BorderLayout.CENTER);
@@ -27,11 +25,48 @@ public class Main extends JFrame{
     }
     class CenterPanel extends JPanel{
         public CenterPanel(){
-            setBackground(Color.LIGHT_GRAY);
-            setLayout(null);
-            setLayout(new FlowLayout(FlowLayout.LEFT));
+            setLayout(new BorderLayout());
+
+            JPanel gaugePanel = new JPanel();
+            gaugePanel.setLayout(new GridLayout(1,5));  // GridLayout으로 게이지 패널들을 수평 배치
+
+            // 게이지 패널들 추가
+            gaugePanel.add(Box.createRigidArea(new Dimension(20, 20)));
+            gaugePanel.add(createGuagePanel(" Cup "));
+            gaugePanel.add(Box.createRigidArea(new Dimension(20, 20)));
+
+            gaugePanel.add(createGuagePanel("Coffee"));
+            gaugePanel.add(Box.createRigidArea(new Dimension(20, 20)));
+            gaugePanel.add(createGuagePanel("Water"));
+            gaugePanel.add(Box.createRigidArea(new Dimension(20, 20)));
+            gaugePanel.add(createGuagePanel("Sugar"));
+            gaugePanel.add(Box.createRigidArea(new Dimension(20, 20)));
+            gaugePanel.add(createGuagePanel("Cream"));
+            gaugePanel.add(Box.createRigidArea(new Dimension(20, 20)));
+
+            add(gaugePanel, BorderLayout.NORTH);  // 게이지 패널들을 North 영역에 추가
+
+            // 이미지 추가
+            ImageIcon coffeeImage = new ImageIcon("coffee.jpg");
+            JLabel imageLabel = new JLabel(coffeeImage);
+            imageLabel.setHorizontalAlignment(JLabel.CENTER);  // 이미지를 가운데 정렬
+            add(imageLabel, BorderLayout.CENTER);  // 이미지를 Center 영역에 추가
+        }
+        private JPanel createGuagePanel(String name){
+            JPanel guagePanel = new JPanel();
+            guagePanel.setLayout(new BorderLayout());
+
+            JLabel nameLabel = new JLabel(name, SwingConstants.CENTER);
+            JProgressBar progressBar = new JProgressBar(JProgressBar.VERTICAL, 0,100);
+            progressBar.setValue(100);
+
+            guagePanel.add(progressBar, BorderLayout.CENTER);  // 게이지를 CENTER에 추가
+            guagePanel.add(nameLabel, BorderLayout.SOUTH);  // 요소 이름을 SOUTH에 추가
+            return guagePanel;
         }
     }
+
+
     class SouthPanel extends JPanel{
         public SouthPanel() {
             setBackground(Color.LIGHT_GRAY);
