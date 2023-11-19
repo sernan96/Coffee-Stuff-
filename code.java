be4ba279b87b4d6f18b600c608f8f4fddf4770b4
Hwang gend <80672346+sernan96@users.noreply.github.com>
2023-11-20 00:30:06 +0900

    [UPDATE] Layout
    JProgressBar 생성
    nameLabel 생성
    후 각 각 재료Panel에 넣어서 
    GridLayout으로 넣어주기

--- code.java
+++ code.java
@@ -1,3 +1,4 @@
+package Main;
 import javax.swing.*;
 import java.awt.*;
 
@@ -13,6 +14,7 @@ public class Main extends JFrame{
         setVisible(true);
     }
     public static void main(String[] args) {
+
         new Main();
     }
     class NorthPanel extends JPanel{
@@ -29,21 +31,58 @@ public class Main extends JFrame{
 
             JPanel gaugePanel = new JPanel();
             gaugePanel.setLayout(new GridLayout(1,5));  // GridLayout으로 게이지 패널들을 수평 배치
-
+            gaugePanel.add(Box.createRigidArea(new Dimension(20, 20)));
             // 게이지 패널들 추가
-            gaugePanel.add(Box.createRigidArea(new Dimension(20, 20)));
-            gaugePanel.add(createGuagePanel(" Cup "));
-            gaugePanel.add(Box.createRigidArea(new Dimension(20, 20)));
+            //cupPanel
+            JPanel cupPanel = new JPanel(new BorderLayout());
+            JProgressBar cupBar = new JProgressBar(JProgressBar.VERTICAL, 0,100);
+            cupBar.setValue(100);
+            cupPanel.add(cupBar, BorderLayout.NORTH);
+            JLabel cupLabel = new JLabel("Cup", SwingConstants.CENTER);
+            cupPanel.add(cupLabel, BorderLayout.SOUTH);
+            gaugePanel.add(cupPanel);
 
-            gaugePanel.add(createGuagePanel("Coffee"));
             gaugePanel.add(Box.createRigidArea(new Dimension(20, 20)));
-            gaugePanel.add(createGuagePanel("Water"));
-            gaugePanel.add(Box.createRigidArea(new Dimension(20, 20)));
-            gaugePanel.add(createGuagePanel("Sugar"));
-            gaugePanel.add(Box.createRigidArea(new Dimension(20, 20)));
-            gaugePanel.add(createGuagePanel("Cream"));
-            gaugePanel.add(Box.createRigidArea(new Dimension(20, 20)));
+            //coffeePanel 추가
+            JPanel coffeePanel = new JPanel(new BorderLayout());
+            JProgressBar coffeeBar = new JProgressBar(JProgressBar.VERTICAL, 0,100);
+            coffeeBar.setValue(100);
+            coffeePanel.add(coffeeBar, BorderLayout.NORTH);
+            JLabel coffeeLabel = new JLabel("Coffee", SwingConstants.CENTER);
+            coffeePanel.add(coffeeLabel, BorderLayout.SOUTH);
+            gaugePanel.add(coffeePanel);
 
+            gaugePanel.add(Box.createRigidArea(new Dimension(20, 20)));
+            //waterPanel 추가
+            JPanel waterPanel = new JPanel(new BorderLayout());
+            JProgressBar waterBar = new JProgressBar(JProgressBar.VERTICAL, 0,100);
+            waterBar.setValue(100);
+            waterPanel.add(waterBar, BorderLayout.NORTH);
+            JLabel waterLabel = new JLabel("Water", SwingConstants.CENTER);
+            waterPanel.add(waterLabel, BorderLayout.SOUTH);
+            gaugePanel.add(waterPanel);
+            
+            gaugePanel.add(Box.createRigidArea(new Dimension(20, 20)));
+            //sugarPanel 추가
+            JPanel sugarPanel = new JPanel(new BorderLayout());
+            JProgressBar sugarBar = new JProgressBar(JProgressBar.VERTICAL, 0,100);
+            sugarBar.setValue(100);
+            sugarPanel.add(sugarBar, BorderLayout.NORTH);
+            JLabel sugarLabel = new JLabel("Sugar", SwingConstants.CENTER);
+            sugarPanel.add(sugarLabel, BorderLayout.SOUTH);
+            gaugePanel.add(sugarPanel);
+            
+            gaugePanel.add(Box.createRigidArea(new Dimension(20, 20)));
+            //creamPanel추가
+            JPanel creamPanel = new JPanel(new BorderLayout());
+            JProgressBar creamBar = new JProgressBar(JProgressBar.VERTICAL, 0,100);
+            creamBar.setValue(100);
+            creamPanel.add(creamBar, BorderLayout.NORTH);
+            JLabel creamLabel = new JLabel("Cream", SwingConstants.CENTER);
+            creamPanel.add(creamLabel, BorderLayout.SOUTH);
+            gaugePanel.add(creamPanel);
+            
+            gaugePanel.add(Box.createRigidArea(new Dimension(20, 20)));
             add(gaugePanel, BorderLayout.NORTH);  // 게이지 패널들을 North 영역에 추가
 
             // 이미지 추가
@@ -51,18 +90,6 @@ public class Main extends JFrame{
             JLabel imageLabel = new JLabel(coffeeImage);
             imageLabel.setHorizontalAlignment(JLabel.CENTER);  // 이미지를 가운데 정렬
             add(imageLabel, BorderLayout.CENTER);  // 이미지를 Center 영역에 추가
-        }
-        private JPanel createGuagePanel(String name){
-            JPanel guagePanel = new JPanel();
-            guagePanel.setLayout(new BorderLayout());
-
-            JLabel nameLabel = new JLabel(name, SwingConstants.CENTER);
-            JProgressBar progressBar = new JProgressBar(JProgressBar.VERTICAL, 0,100);
-            progressBar.setValue(100);
-
-            guagePanel.add(progressBar, BorderLayout.CENTER);  // 게이지를 CENTER에 추가
-            guagePanel.add(nameLabel, BorderLayout.SOUTH);  // 요소 이름을 SOUTH에 추가
-            return guagePanel;
         }
     }
 
